a820941c8135482064cd2b1cef7c1dc29c3b41c5
Kolja Grosse <kolja.grosse@gmail.com>
2026-07-13 19:54:07 +0200

    Format terraform, add local preview config

--- infra/s3.tf
+++ infra/s3.tf
@@ -9,8 +9,8 @@ resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
   block_public_acls       = true
-  block_public_policy      = true
-  ignore_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
